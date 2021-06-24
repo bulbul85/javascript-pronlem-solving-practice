aa8d59543abfe60bfaf8115fa00a790c52c37306
Bulbul Niaz <83117381+bulbul85@users.noreply.github.com>
2021-06-24 10:11:28 -0700

    Add files via upload

---
+++ coding task#9TOPIC DOM/AddEventListener/app.js
@@ -0,0 +1,3 @@
+document.getElementById("button").addEventListener("click", function helloWorld(){
+    alert("Hello!");
+    });
---
+++ coding task#9TOPIC DOM/JSDOM/app.js
@@ -0,0 +1,11 @@
+
+// 2) Problem Name: QuerySelector
+
+
+function styleFunc() {
+
+    document.getElementsByClassName("div-class")[0].style.border = "10px solid blue";
+    document.getElementsByClassName("div-class")[0].style.color = "red";
+    document.getElementsByClassName("div-class")[0].className = "changed-class";
+
+}   
